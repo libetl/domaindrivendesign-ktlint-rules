63bb36201a2a0d1a00fa2c78c5c97c043f60cced
lionel benychou <lbenychou@egencia.com>
2023-09-26 14:27:22 +0200

    3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 import java.net.URI
 
 group = "org.toile-libre.libe"
-version = "3.0.0"
+version = "3.0.1"
 
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
